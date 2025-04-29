7de4804598281ba92204511c39b4a186b2003be7
rihab-benali <rihabbenali5@gmail.com>
2025-04-29 11:33:07 +0100

    main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from model_loader import load_my_model
 from PIL import Image
 import numpy as np
+from preprocess import preprocess_image
 
 if __name__ == "__main__":
     print("🔁 Chargement du modèle...")
     model = load_my_model()
 
-    # Dummy test avec une image random (à remplacer par une vraie image si besoin)
-    img = Image.new("RGB", (256, 256))
-    from preprocess import preprocess_image
+    img = Image.new("RGB", (256, 256))  # Replace this with a real image for better results
     arr = preprocess_image(img)
 
-    input_batch = np.expand_dims(arr, axis=0)
+    input_batch = np.expand_dims(arr, axis=0)  # Shape should be (1, 256, 256, 3)
+    print("📏 Input batch shape:", input_batch.shape)
+
     predictions = model.predict(input_batch)
     print("✅ Prédiction:", predictions)
